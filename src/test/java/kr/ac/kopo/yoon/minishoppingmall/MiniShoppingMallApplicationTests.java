4952d1f7478a1171b4bb5b0cc7d7ade53ee9ee43
jaeyoon2250 <jaeyoon2250@gmail.com>
2026-06-28 04:16:36 +0900

    MiniMallProject

---
+++ src/test/java/kr/ac/kopo/yoon/minishoppingmall/MiniShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.yoon.minishoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniShoppingMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
